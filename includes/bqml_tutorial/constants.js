e485559ade211a47606f4230c69a6af5cfa344ee
Shingo Furuyama <shingofuruyama@google.com>
2022-11-24 01:52:56 +0900

    mod files

--- includes/bqml_tutorial/constants.js
+++ includes/bqml_tutorial/constants.js
@@ -1,6 +1,6 @@
 const date = new Date();
 const execution_time = date.getTime();
 const penguins_model_simple_name = "penguins_model";
-const penguins_model_versioned_name = `${penguins_model_simple_name}_${execution_time}`;
+const penguins_model_versioned_name = `${dataform.projectConfig.defaultDatabase}.${dataform.projectConfig.defaultSchema}.${penguins_model_simple_name}_${execution_time}`;
 
 module.exports = { execution_time, penguins_model_simple_name, penguins_model_versioned_name};
